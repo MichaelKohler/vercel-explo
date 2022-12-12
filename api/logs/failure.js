673c47f0c50f5cf1a562ddee3fccb8424a2a2f98
Michael Kohler <me@michaelkohler.info>
2022-12-12 22:33:34 +0100

    Add console.error

--- api/logs/failure.js
+++ api/logs/failure.js
@@ -1,5 +1,7 @@
 export default function handler(request, response) {
   console.log("I'm gonna be a failure!");
+  console.log("Before throwing, test console.error:")
+  console.error(new Error("CONSOLE_ERROR"));
 
   const someError = new Error("Oh no!");
 
